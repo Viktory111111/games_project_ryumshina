14d203160b4873daaaf0f3649c5e869432dcf43c
Виктория Рюмшина <viktoriarumsina13@gmail.com>
2025-10-17 16:10:38 +0300

    Выполнен практос жестко

---
+++ games_project_Ryumshina/scripts/VD_main.py
@@ -0,0 +1,10 @@
+from games_project_Ryumshina.scripts.VD_games import play_game
+
+def main():
+    """Главная функция - точка входа в приложение."""
+    print("Запуск игрового проекта...")
+    play_game()
+    print("Игра завершена. До свидания!")
+
+if __name__ == "__main__":
+    main()
