c7003b0b622e7960d4e312931c34ffbb9b812a6c
Виктория Рюмшина <viktoriarumsina13@gmail.com>
2025-11-07 00:27:51 +0300

    Реализация игры НОД (VD-gcd)

--- VD_games/VD_games/cli.py
+++ VD_games/VD_games/cli.py
@@ -10,17 +10,15 @@ def welcome_user():
 
 def run_game(name, question_generator, game_name):
     rounds_count = 3
-    
     for _ in range(rounds_count):
         question, correct_answer = question_generator()
         print(f"Question: {question}")
         user_answer = prompt.string("Your answer: ")
-        
         if user_answer == correct_answer:
             print("Correct!")
         else:
-            print(f"'{user_answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"'{user_answer}' is wrong answer ;(.")
+            print(f"Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             return
-    
     print(f"Congratulations, {name}!")
